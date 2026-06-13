e9d0bafdd1d748225aec3327489c363122d7c4ca
G4sTt <sasha.mareyev@gmail.com>
2026-06-13 11:55:11 +0300

    добавил файл для ввода параметров тела

---
+++ main/states.py
@@ -0,0 +1,8 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class BodyMeasurements(StatesGroup):
+    waiting_for_bicep = State()       # Ожидание ввода обхвата бицепса
+    waiting_for_forearm = State()     # Ожидание ввода обхвата предплечья
+    waiting_for_shoulders = State()   # Ожидание ввода обхвата плеч
+    waiting_for_chest = State()       # Ожидание ввода обхвата груди
+    waiting_for_hips = State()        # Ожидание ввода обхвата бедер
