dbae62c0d0cdd6186c97489cf31ddebe97f0c85a
G4sTt <sasha.mareyev@gmail.com>
2026-06-16 16:42:30 +0300

    states обновил

--- main/states.py
+++ main/states.py
@@ -5,4 +5,7 @@ class BodyMeasurements(StatesGroup):
     waiting_for_forearm = State()     # Ожидание ввода обхвата предплечья
     waiting_for_shoulders = State()   # Ожидание ввода обхвата плеч
     waiting_for_chest = State()       # Ожидание ввода обхвата груди
-    waiting_for_hips = State()        # Ожидание ввода обхвата бедер
+    waiting_for_hip = State()        # Ожидание ввода обхвата бедер
+    waiting_for_calf = State()
+    waiting_for_waist = State()
+    waiting_for_ass = State()
